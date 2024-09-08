43057db013d5085aa8ed77ae669f05c67eaadabe
Damien Sedgwick <damienksedgwick@gmail.com>
2024-09-08 21:10:25 +0100

    data is decoding, just need to decode blocks, commits and ??

--- main.go
+++ main.go
@@ -11,26 +11,33 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#account]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-// 2024/09/08 17:28:43 rawMap: map[op:1 t:#commit]
-
 type Header struct {
 	Type      string `cbor:"t"`
 	Operation uint8  `cbor:"op"`
 }
 
+type SubscribeReposCommit struct {
+	Blobs      []byte                          `cbor:"blobs"`
+	Blocks     []byte                          `cbor:"blocks"`
+	Commit     []byte                          `cbor:"commit"`
+	Operations []SubscribeReposCommitOperation `cbor:"ops"`
+	Prev       *cid.Cid                        `cbor:"prev,omitempty"`
+	Rebase     bool                            `cbor:"rebase"`
+	Repo       string                          `cbor:"repo"`
+	Rev        string                          `cbor:"rev"`
+	Sequence   int64                           `cbor:"seq"`
+	Since      string                          `cbor:"since"`
+	Time       time.Time                       `cbor:"time"`
+	TooBig     bool                            `cbor:"tooBig"`
+}
+
 type SubscribeReposCommitOperationAction string
+
+type SubscribeReposCommitOperation struct {
+	Path   string                              `cbor:"path"`
+	Action SubscribeReposCommitOperationAction `cbor:"action"`
+	CID    []byte                              `cbor:"cid,omitempty"`
+}
 
 const (
 	Create SubscribeReposCommitOperationAction = "create"
@@ -38,55 +45,12 @@ const (
 	Delete SubscribeReposCommitOperationAction = "delete"
 )
 
-type SubscribeReposCommitOperation struct {
-	Path   string                              `cbor:"path"`
-	Action SubscribeReposCommitOperationAction `cbor:"action"`
-	CID    *cid.Cid                            `cbor:"cid,omitempty"`
-}
-
-type SubscribeReposCommit struct {
-	Commit     cid.Cid                         `cbor:"commit"`
-	Operations []SubscribeReposCommitOperation `cbor:"ops"`
-	Prev       *cid.Cid                        `cbor:"prev,omitempty"`
-	Rebase     bool                            `cbor:"rebase"`
-	Repo       string                          `cbor:"repo"`
-	Sequence   int64                           `cbor:"seq"`
-	Time       time.Time                       `cbor:"time"`
-	TooBig     bool                            `cbor:"tooBig"`
-}
-
-type SubscribeReposHandle struct {
-	DID      string    `cbor:"did"`
-	Handle   string    `cbor:"handle"`
-	Sequence int64     `cbor:"seq"`
-	Time     time.Time `cbor:"time"`
-}
-
-type SubscribeReposTombstone struct {
-	DID      string    `cbor:"did"`
-	Sequence int64     `cbor:"seq"`
-	Time     time.Time `cbor:"time"`
-}
-
-type SubscribeReposAccount struct {
-	Sequence int64 `cbor:"seq"`
-}
-
-type SubscribeReposIdentity struct {
-	Sequence int64 `cbor:"seq"`
-}
-
-type SubscribeRepos struct {
-	Header Header
-	Body   interface{}
-}
-
 // "wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos"
 // "wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos?cursor={}"
 // "wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos?cursor=1537553850"
 
 func main() {
-	conn, err := connect("wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos?curso=1537553850", nil)
+	conn, err := connect("wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos?curso=1543524352", nil)
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
 	}
@@ -106,7 +70,7 @@ func main() {
 		}
 	}()
 
-	events := make(chan SubscribeRepos)
+	events := make(chan SubscribeReposCommit)
 
 	go func() {
 		defer close(events)
@@ -120,7 +84,7 @@ func main() {
 	}()
 
 	for event := range events {
-		log.Printf("event: %+v", event)
+		log.Printf("event: %v", event)
 	}
 }
 
@@ -142,15 +106,21 @@ func read(conn *websocket.Conn) ([]byte, error) {
 	return msg, nil
 }
 
-func parse(msg []byte) (SubscribeRepos, error) {
+func parse(msg []byte) (SubscribeReposCommit, error) {
 	var header Header
 	decoder := cbor.NewDecoder(bytes.NewReader(msg))
 	if err := decoder.Decode(&header); err != nil {
-		return SubscribeRepos{}, err
+		return SubscribeReposCommit{}, err
 	}
 
-	return SubscribeRepos{
-		Header: header,
-		Body:   nil,
-	}, nil
+	if header.Type != "#commit" {
+		return SubscribeReposCommit{}, nil
+	}
+
+	var commit SubscribeReposCommit
+	if err := decoder.Decode(&commit); err != nil {
+		return SubscribeReposCommit{}, err
+	}
+
+	return commit, nil
 }
